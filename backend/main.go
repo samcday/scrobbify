8f972942c10b46eb383ab21f6c9bf8925ed5d010
Sam Day <me@samcday.com>
2017-10-04 22:35:27 -0700

    Playing around with aws-lambda-shim-go

---
+++ backend/main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"github.com/eawsy/aws-lambda-go-core/service/lambda/runtime"
+)
+
+func Handle(evt interface{}, ctx *runtime.Context) (string, error) {
+	return "Hello, world!", nil
+}
