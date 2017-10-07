bdfd3ba506922c1ccdabcef5423065a1e4b02f98
Sam Day <me@samcday.com>
2017-10-07 08:32:37 -0700

    Use github.com/zmb3/spotify lib

--- backend/main.go
+++ backend/main.go
@@ -1,42 +1,43 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
 	"net/http"
-	"net/url"
-	"os"
 
 	"github.com/eawsy/aws-lambda-go-net/service/lambda/runtime/net"
 	"github.com/eawsy/aws-lambda-go-net/service/lambda/runtime/net/apigatewayproxy"
+	"github.com/zmb3/spotify"
 )
+
+// var spotifyAccountBaseUrl = "https://accounts.spotify.com"
+
+var spotifyAuth spotify.Authenticator
 
 // Handle is the exported handler called by AWS Lambda.
 var Handle apigatewayproxy.Handler
 
 func init() {
+	spotifyAuth = spotify.NewAuthenticator("http://localhost:3000/spotifycallback", spotify.ScopeUserReadCurrentlyPlaying, spotify.ScopeUserReadEmail)
+
 	ln := net.Listen()
 	Handle = apigatewayproxy.New(ln, nil).Handle
-	go http.Serve(ln, http.DefaultServeMux)
 
 	http.HandleFunc("/login", func(w http.ResponseWriter, r *http.Request) {
-		qs := url.Values{}
-		qs.Add("response_type", "code")
-		qs.Add("client_id", os.Getenv("SPOTIFY_CLIENT_ID"))
-		qs.Add("scope", "user-read-currently-playing user-read-email")
-		qs.Add("redirect_uri", "http://localhost:3000/spotifycallback")
-		qs.Add("state", "bacon")
-
-		http.Redirect(w, r, "https://accounts.spotify.com/authorize?"+qs.Encode(), http.StatusTemporaryRedirect)
+		http.Redirect(w, r, spotifyAuth.AuthURL("bacon"), http.StatusTemporaryRedirect)
 	})
 
 	http.HandleFunc("/spotifycallback", func(w http.ResponseWriter, r *http.Request) {
-		state := r.URL.Query().Get("state")
-		if state != "bacon" {
-			fmt.Fprint(w, "Bad state")
+		token, err := spotifyAuth.Token("bacon", r)
+		if err != nil {
+			http.Error(w, fmt.Sprintf("Auth error: %s", err), http.StatusNotFound)
 			return
 		}
 
-		json.NewEncoder(w).Encode(r.URL.Query())
+		client := spotifyAuth.NewClient(token)
+
+		cp, err := client.PlayerCurrentlyPlaying()
+		fmt.Fprintf(w, "Yay! %s", cp.Item.Name)
 	})
+
+	go http.Serve(ln, http.DefaultServeMux)
 }
